5d8850f1c2b42df67677da03ea09ecc3680a6d68
Ludo Vlieger <lvlieger@twilio.com>
2022-10-14 09:16:24 -0400

    Updated deps for Plugins CLI v5

--- pause-recording-plugin/src/PauseRecordingPlugin.js
+++ pause-recording-plugin/src/PauseRecordingPlugin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { VERSION, Notifications, NotificationType } from '@twilio/flex-ui';
-import { FlexPlugin } from 'flex-plugin';
+import { FlexPlugin } from '@twilio/flex-plugin';
 
 import reducers, { namespace } from './states';
 import PauseRecordingButton from './components/RecordingButton/PauseRecordingButton';
